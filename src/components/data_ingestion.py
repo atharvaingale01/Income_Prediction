caf6e4e2b16035fe0bb9cfc912bea00dc52c8181
atharvaingale01 <atharvaingale01@gmail.com>
2023-10-17 14:46:21 +0530

    sklearn pipeline built

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -45,6 +45,6 @@ class DataIngestion:
             logging.info("Error occured in data ingestion")
             raise CustumException(e,sys)
 
-if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+# if __name__ == "__main__":
+#     obj = DataIngestion()
+#     obj.initiate_data_ingestion()
